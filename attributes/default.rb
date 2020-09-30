613a765de4b66dfe80be234ec9a49be7fc87fedc
Eric Kuiper <ekuiper@schubergphilis.com>
2020-09-30 14:26:15 +0200

    remove var

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default['sysinternals']['url']               = 'http://download.sysinternals.com/files/SysinternalsSuite.zip'
 default['sysinternals']['install_dir']       = 'C:\SBP\SysinternalsSuite'
 default['sysinternals']['temp']              = 'c:\Windows\Temp'
-default['sysinternals']['bginfo_config_url'] = nil
